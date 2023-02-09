95a83f1126d3ed9357e133a2b48cfe18a945f7b4
ArtemKomolin <komolin.artem@mail.ru>
2023-02-09 20:01:18 +0300

    Add images, changed logic

--- sapsite/sapy/static/sapy/js/script.js
+++ sapsite/sapy/static/sapy/js/script.js
@@ -2,8 +2,10 @@ for (let i = 1; i <= 100; i++) {
   $(`#scale-${i}`).click(function() {
     $(`#scale-${i}`).addClass('button-clicked');
 });
+  $(`a${i}`).hide();
   $(`#scale2-${i}`).click(function() {
-    $(`#scale2-${i}`).append( $( `strong${i}` ) );
+    $(`a${i}`).show();
+    $(`#scale2-${i}`).append( $( `a${i}` ) );
 });
 }
 
